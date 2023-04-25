89292ce5d31db21797dbf66ab1a3c027c7a30123
achraf <achrafqaroun03@gmail.com>
2023-04-25 22:50:25 +0100

    Initial commit

---
+++ src/test/java/ma/emsi/achrafmvc/AchrafMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.achrafmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchrafMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
